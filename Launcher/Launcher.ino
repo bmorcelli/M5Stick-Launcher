6b96ae744b93a6f2686426033f9c37910e8b46c2
bmorcelli <104320209+bmorcelli@users.noreply.github.com>
2024-02-12 17:39:05 -0300

    Update Launcher.ino

--- Launcher/Launcher.ino
+++ Launcher/Launcher.ino
@@ -385,7 +385,7 @@ void loop() {
 
       // Verify if the first 3 bytes are different from 0xE9 0x05 0x02 (meaning that the file have Bootloader, partitions and application)
       //
-      if (firstThreeBytes[0] == 0xE9 == firstThreeBytes[1] == 0x05 && firstThreeBytes[2] == 0x02) {
+      if (firstThreeBytes[0] == 0xE9 && firstThreeBytes[1] == 0x05 && firstThreeBytes[2] == 0x02) {
         Serial.println("First 3 bytes are 0xE9 0x05 0x02");
         updateFromFS(SD,PreFolder + "/" + fileList[selectIndex - folderListCount], LNSD_CS_PIN);
         ESP.restart();
